756ca242332aa2dd2fb1f7b3bff4ef8876ff40f2
3rdto1stworld-dot <3rdto1stworld@gmail.com>
2025-12-16 14:07:09 +0100

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-// Theme toggler (future UI hook)
-function setTheme(name) {
-  document.documentElement.setAttribute("data-theme", name);
-}
-
-// Example:
-// setTheme("dark");
-// setTheme("server");
